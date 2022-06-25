dd434f42651f001a7577984646790fb9985425ac
ArtieHuang <3312002405@qq.com>
2022-06-25 16:09:45 +0800

    rust-rpc

--- tonic1/src/client.rs
+++ tonic1/src/client.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!11");
+use tonic::Request;
+use records::recorder_client::RecorderClient;
+use records::RecordRequest;
+
+pub mod records{
+    tonic::include_proto!("records");
 }
+
+
+#[tokio::main]
+async fn main() ->Result<(),Box<dyn std::error::Error>>{
+    let mut client =RecorderClient::connect("http://[::1]:50050").await?;
+    let request=Request::new(
+        RecordRequest{
+            user_name: "huang".to_string(),
+            user_age:30,
+        }
+    );
+    let response =client.send_message(request).await?;
+    println!("{:#?}", response);
+
+    Ok(())
+
+}
+
+
--- tonic1/src/server.rs
+++ tonic1/src/server.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello,  !");
+
+use tonic::{Request, Response, Status};
+use tonic::transport::Server;
+use records::recorder_server::{Recorder, RecorderServer};
+use records::{RecordRequest,RecordResponse};
+
+
+
+pub mod records{
+    tonic::include_proto!("records");
+}
+
+#[derive (Debug,Default)]
+pub struct RecorderService {
+}
+
+#[tonic::async_trait]
+impl Recorder for RecorderService {
+    async fn send_message(
+        &self,
+        request: Request<RecordRequest>
+    )->Result<Response<RecordResponse>,Status>{
+        println!("Request{:#?}",request);
+
+        let req =request.into_inner();
+
+        let response =RecordResponse{
+            successful:true,
+            message:format!("user {} is {} years old ",req.user_name,req.user_age).into()
+        };
+        Ok(Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() ->Result<(),Box<dyn std::error::Error>>{
+    let addr="[::1]:50050".parse()?;
+    let recorder:RecorderService=RecorderService::default();
+    println!("Recorder listening on {}!",addr);
+
+    Server::builder()
+        .add_service(RecorderServer::new(recorder))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
